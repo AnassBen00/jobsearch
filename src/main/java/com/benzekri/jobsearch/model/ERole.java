af2d287271f021c4f1ffdfaef85e33a0a1193886
Benzekri <benzekrianass@gmail.com>
2024-08-21 15:49:31 +0200

    implementing spring security config

---
+++ src/main/java/com/benzekri/jobsearch/model/ERole.java
@@ -0,0 +1,7 @@
+package com.benzekri.jobsearch.model;
+
+public enum ERole {
+    ROLE_EMPLOYER,
+    ROLE_JOBSEEKER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/benzekri/jobsearch/security/jwt/JwtUtils.java
@@ -0,0 +1,65 @@
+package com.benzekri.jobsearch.security.jwt;
+
+import com.benzekri.jobsearch.security.services.UserDetailsImpl;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+import java.security.Key;
+
+@Component
+public class JwtUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${benzekri.jobsearch.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${benzekri.jobsearch.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String generateJwtToken(Authentication authentication){
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setExpiration(new Date((new Date()).getTime()+jwtExpirationMs))
+                .setIssuedAt(new Date())
+                .signWith(key(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getUsernameFromJwtToken(String token){
+        return Jwts.parserBuilder().setSigningKey(key()).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String token){
+        try{
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(token);
+            return true;
+        }catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
+    }
+
+}
